09ebea6fc62fa07e6dbce088ae94383e3d1f61e3
rprieto <choicesmade@gmail.com>
2014-04-25 10:53:12 +1000

    Better integration tests - all good!

--- test/integration.spec.js
+++ test/integration.spec.js
@@ -3,6 +3,7 @@ var sinon        = require('sinon');
 var fs           = require('fs');
 var path         = require('path');
 var rimraf       = require('rimraf');
+var touch        = require('touch');
 var regen        = require('../src/index');
 
 describe('Integration', function() {
@@ -10,38 +11,45 @@ describe('Integration', function() {
   before(cleanup);
   after(cleanup);
 
-  it('builds all dest files from scratch', function(done) {
-    regen({
-      cwd: path.join(__dirname, 'data'),
-      src: '**/*.txt',
-      dest: '../tmp/$path/$name.rand',
-      process: 'echo "initial" > $dest'
-    }, done);
+  it('builds all destination files from scratch', function(done) {
+    rebuild('initial', function() {
+      checkContents('./tmp/one.rand', 'initial');
+      checkContents('./tmp/folder/two.rand', 'initial');
+      done();
+    });
   });
 
-  it('checks they have the correct content', function() {
-    checkContents('./tmp/one.rand', 'initial');
-    checkContents('./tmp/folder/two.rand', 'initial');
+  it('does nothing if rebuilding straight away', function(done) {
+    rebuild('round two', function() {
+      checkContents('./tmp/one.rand', 'initial');
+      checkContents('./tmp/folder/two.rand', 'initial');
+      done();
+    });
   });
 
-  it('tries to rebuild them again', function(done) {
-    regen({
-      cwd: path.join(__dirname, 'data'),
-      src: '**/*.txt',
-      dest: '../tmp/$path/$name.rand',
-      process: 'echo "rebuilt" > $dest'
-    }, done);
-  });
-
-  it('checks it did nothing', function() {
-    checkContents('./tmp/one.rand', 'initial');
-    checkContents('./tmp/folder/two.rand', 'initial');
+  it('rebuilds files if the source file has changed', function(done) {
+    var futureDate = new Date().getTime() + 1000;
+    touch.sync(path.join(__dirname, './data/one.txt'), {time: futureDate});
+    rebuild('round three', function() {
+      checkContents('./tmp/one.rand', 'round three');
+      checkContents('./tmp/folder/two.rand', 'initial');
+      done();
+    });
   });
 
 });
 
 function cleanup(done) {
   rimraf(path.join(__dirname, 'tmp'), done);
+}
+
+function rebuild(contents, done) {
+  regen({
+    cwd: path.join(__dirname, 'data'),
+    src: '**/*.txt',
+    dest: '../tmp/$path/$name.rand',
+    process: 'echo "' + contents +'" > $dest'
+  }, done);
 }
 
 function checkContents(file, contents) {
